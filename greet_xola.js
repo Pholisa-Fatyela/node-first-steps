37af32b303162f5f9d1df7fcaf663529251184a8
Pholisa-Fatyela <pholisa@projectcodex.co>
2018-07-25 09:16:32 +0200

    updated greet xola

--- greet_xola.js
+++ greet_xola.js
@@ -1,9 +1,24 @@
-"use strict";
+'use strict';
 
+const greet = require('./greet.js');
 const chalk = require('chalk');
-//import the greet module that is in the current folder
-const greet = require('./greet');
 
-// console.log(greet('Xola'))
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage);
+console.log(greet('Xola'));
+
+var message = chalk.bgGreen.black(greet("Xola"));
+console.log(message);
+
+var figlet = require('figlet');
+ 
+figlet.text('Xola', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
